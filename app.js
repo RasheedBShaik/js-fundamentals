ff3d7f0c32e020b176098c443f8c43a7e31406fe
RasheedBShaik <rasheedbasha.2004@gmail.com>
2024-09-29 21:15:55 +0530

    return early patterns from functions

--- app.js
+++ app.js
@@ -520,3 +520,13 @@ function islessex2(a, b) {
   return a < b;
 }
 console.log(islessex2(20, 15));
+
+////returning early patterns from functions
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2, 2));
+// console.log(abTest(1, 1));
