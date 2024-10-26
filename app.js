1ecb8ee25a9c0cc17ed5ca1881d2761f75e7d065
RasheedBShaik <rasheedbasha.2004@gmail.com>
2024-10-26 20:20:42 +0530

    higher order arrow functions

--- app.js
+++ app.js
@@ -931,3 +931,27 @@ console.log(magic());
 
 const myconcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myconcat([1, 2], [3, 4, 5]));
+
+//write higher order arrow fuctions
+
+// first example
+
+const realNumArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+  const squaredIntegers = arr
+    .filter((num) => Number.isInteger(num) && num > 0)
+    .map((x) => x * x);
+  return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumArray);
+console.log(squaredIntegers);
+
+// second example
+
+const increment = (number, value = 1) => {
+  return number + value;
+};
+console.log(increment(1, 4));
+console.log(increment(3));
